90682cea73c487e5436614c80ecff4a01e7f4f24
Manasvi05Dadhich <manasvidadhich05@gmail.com>
2026-02-27 19:17:41 +0530

    Refactor certificateRegistry contract to include certificate issuance functionality and events

--- contracts/src/certificateReg.sol
+++ contracts/src/certificateReg.sol
@@ -1,9 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 contract certificateRegistry {
+
     address public owner ; 
-
-
     modifier onlyOwner () {
         require(msg.sender== owner, 'Not Admin' );
         _ ;  
@@ -28,4 +28,34 @@ contract certificateRegistry {
           require(issuers[msg.sender], "Not issuer");
         _;
     }
+
+    struct Certificate {
+        address issuer;
+        uint256 issuedAt;
+        bool isRevoked;
+        
+        uint256 revokedAt;
+    }
+
+    mapping(bytes32 => Certificate) public certificates ;
+
+    event certIssued(bytes32 hash , address issuer);
+    event  certRevoked(bytes32 hash, address issuer);
+
+
+    function issueCert(bytes32 hash) external onlyIssuer{
+        require(hash != bytes32(0), "empty hash");
+        require(certificates[hash].issuer== address(0), "duplicate");
+
+        certificates[hash] = Certificate({
+            issuer : msg.sender,
+            issuedAt : block.timestamp,
+            isRevoked : false,
+             revokedAt : 0
+        });
+
+        
+        emit CertificateIssued(hash, msg.sender);
+    }
+    
 }
